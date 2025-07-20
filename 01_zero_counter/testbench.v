72d12f114c04880caaabfb8c252ebcb5d027374e
m0d3rn1ca <jaykonstanta@mail.ru>
2025-07-21 01:53:04 +0300

    zero_counter reversed impl

--- 01_zero_counter/testbench.v
+++ 01_zero_counter/testbench.v
@@ -1,33 +1,50 @@
-/* verilator lint_off UNUSEDSIGNAL */
+/* verilator lint_off WIDTHTRUNC */
 
 module testbench;
     
-parameter IN_W  = 8;
-parameter OUT_W = $clog2(IN_W + 1);
+    localparam IN_W    = 8;
+    localparam OUT_W   = $clog2(IN_W + 1);
 
-reg  [IN_W - 1:0]  in;
-wire [OUT_W - 1:0] out;
+    reg  [IN_W - 1:0]  in_low;
+    wire [OUT_W - 1:0] out_low;
+    reg  [IN_W - 1:0]  in_high;
+    wire [OUT_W - 1:0] out_high;
+    reg [IN_W - 1:0]   val_low;
+    reg [IN_W - 1:0]   val_high;
+    reg [OUT_W - 1:0]  i;
+    
+    zero_counter #(.IN_W(IN_W), .REVERSE(0)) zc_high (
+        .in(in_high),
+        .out(out_high)
+    );
 
-zero_counter #(.IN_W(IN_W)) unit (
-    .in(in),
-    .out(out)
-);
+    zero_counter #(.IN_W(IN_W), .REVERSE(1)) zc_low (
+        .in(in_low),
+        .out(out_low)
+    );
 
-initial begin
-    $dumpfile("trace.vcd");
-    $dumpvars();
+    initial begin
+        $dumpfile("trace.vcd");
+        $dumpvars();
 
-    in = 8'b11110000;
-    #10;
-    in = 8'b00000111;
-    #10;
-    in = 8'b00100111;
-    #10;
-    in = 8'b00000000;
-    #10;
-    in = 8'b11111111;
-    #10;
-    $finish(); 
-end
+        val_low = $random();
+        val_low[0] = 1;
+        val_high = $random();
+        val_high[IN_W - 1] = 1;
+
+        for (i = 0; i <= IN_W; i = i + 1) begin
+            in_high = val_high;
+            in_low = val_low;
+            #5
+            $display("(High) Vector '%b' has%d. Correct is%d", val_high, out_high, i);
+            $display("(Low) Vector '%b' has%d. Correct is%d", val_low, out_low, i);
+            val_high = (val_high >> 1);
+            val_low = (val_low << 1);
+            if ((out_high != i) || (out_low != i)) begin
+                $stop;
+            end
+        end
+        $finish();
+    end
 
 endmodule
--- 01_zero_counter/zero_counter.v
+++ 01_zero_counter/zero_counter.v
@@ -2,6 +2,7 @@
 
 module zero_counter 
 #(
+    parameter REVERSE = 0,
     parameter IN_W = 8,
     parameter OUT_W = $clog2(IN_W + 1)
 )
@@ -10,18 +11,29 @@ module zero_counter
     output [OUT_W - 1:0] out
 );
 
-wire [OUT_W - 1:0] buff [IN_W:0]; // Набор проводов для хранения промежуточных результатов. На один больше!
+    wire [OUT_W - 1:0] buff [IN_W:0]; // Набор проводов для хранения промежуточных результатов. На один больше!
+    wire [IN_W - 1:0]  permutation; // Набор входных проводов для перестановки
+    
+    assign buff[0] = 0;
+    
+    genvar i;
 
-assign buff[0] = 0;
+    generate
+        if (REVERSE == 0) begin: high_zeros
+            assign permutation = in;
+        end else begin: low_zeros
+            for (i = 0; i < IN_W ; i = i + 1) begin: low_zeros_loop
+                assign permutation[i] = in[IN_W - 1 - i];
+            end
+        end
+    endgenerate  
 
-genvar i;
+    generate
+        for (i = 0; i < IN_W; i = i + 1 ) begin: loop
+            assign buff[i + 1] = permutation[i] ? 0 : (buff[i] + 1);
+        end
+    endgenerate
 
-generate
-    for (i = 0; i < IN_W; i = i + 1 ) begin
-        assign buff[i + 1] = in[i] ? 0 : (buff[i] + 1);
-    end
-endgenerate
-
-assign out = buff[IN_W];
+    assign out = buff[IN_W];
 
 endmodule // Самый дешёвый по транзисторам вариант, но самый долгий? Из-за этого verilator кидает UNOPTFLAT
